2aa59b928b675ceab4af102409777a0724696f99
PedroMarMol <martosmoleropedro@gmail.com>
2022-05-16 12:12:01 +0200

    completed css, html and added button that runs the function in js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-function book(title, author, pages, read) {
-            this.title = title
-            this.author = author
-            this.pages = pages
-            this.read = read
-            this.info = function() {
-                return(title + " by " + author + ", " + pages + ", " + read)
-            }
-        }
+const bookCheck = document.querySelector(".book");
+const titleCheck = document.querySelector(".title");
+const pagesCheck = document.querySelector(".pages");
 
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 
-                            '295 pages', 'not read yet');
+const formCheck = document.querySelector(".validform");
+
+// Function runs when user clicks the submit button
+
+const submit = document.querySelector("#submit")
+submit.addEventListener('click', addBookToLibrary)
+
+
+
+// let myLibrary = [];
+
+// // function book(title, author, pages, read) {
+// //             this.title = title
+// //             this.author = author
+// //             this.pages = pages
+// //             this.read = read
+// //             this.info = function() {
+// //                 return(title + " by " + author + ", " + pages + ", " + read)
+// //             }
+// //         }
+
+// // function addBookToLibrary() {
+// //     myLibrary.push();
+// // }
+
+// // const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 
+// //                             '295 pages', 'not read yet');
